57d38ac809f0415a263790a6b33c0d5a3cb4e631
keveniskander <keven.iskander@hotmail.com>
2020-11-24 17:18:38 -0500

    Board class created

--- nQueen.py
+++ nQueen.py
@@ -9,3 +9,9 @@ Final Project
 Authors:  Keven Iskander, Carla Castaneda, Nicole Laslavic, Alexander Francis
 __updated__ = "2020-11-24"
 ---------
+"""
+
+class Board:
+    def __init__(self):
+        self.table = []
+        
