769d84c8f260fd22668b481a2bf27f20604aeb37
keveniskander <59800096+keveniskander@users.noreply.github.com>
2020-11-24 17:13:55 -0500

    Create nQueen.py

---
+++ nQueen.py
@@ -0,0 +1,11 @@
+"""
+-------------------------------------------------------
+nQueen.py
+Places N (int) queens on any given NxN chessboard so 
+that no two queens are able to attack each other. 
+-------------------------------------------------------
+CP468
+Final Project
+Authors:  Keven Iskander, Carla Castaneda, Nicole Laslavic, Alexander Francis
+__updated__ = "2020-11-24"
+---------
