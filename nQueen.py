62a7078f5527404ecc0079e90e81c36b0739c801
carlaac99 <cast4730@mylaurier.ca>
2020-12-04 16:34:34 -0500

    fixed some things

--- nQueen.py
+++ nQueen.py
@@ -11,6 +11,8 @@ __updated__ = "2020-11-24"
 ---------
 """
 import random
+from colorama import Fore, Back, Style
+
 
 class Queen:
     value = 0
@@ -45,9 +47,14 @@ class Board:
         return
 
     def add_queens(self):
+        random_list=list(range(0,self.n))
+        random.shuffle(random_list)
 
         for i in range (len(self.table)):
-            random_row=random.randint(0,self.n-1)
+            random_row=random_list.pop()
+            # random_row=random.randint(0,self.n-1)
+            
+            print("random item from list:")
 
             self.table[random_row][i]=1
         
@@ -152,19 +159,18 @@ class Board:
         for i in range(len(self.queens)):
             row = self.queens[i].row
             col = self.queens[i].col
-            print("Row: ", row)
-            print("Col: ", col)
+            # print("Row: ", row)
+            # print("Col: ", col)
             diag_conflicts = self.conflicts_diag(row, col)
             cardinal_conflicts = self.conflicts_cardinal(row, col)
             conflicts = diag_conflicts + cardinal_conflicts
             self.queens[i].conflicts = conflicts
-            print("Conflicts: ", conflicts)
+            # print("Conflicts: ", conflicts)
         return
 
-    def current_queen_conflicts(self,row,col,queen):
+    def current_queen_conflicts(self,row,col):
         
-        row = row
-        col = col
+
         # print("Row: ", row)
         # print("Col: ", col)
         diag_conflicts = self.conflicts_diag(row, col)
@@ -184,36 +190,54 @@ class Board:
 
     # #max_steps = number of steps allowed before giving up
     def min_conflicts(self, max_steps):
+        max_steps=0
 
-        list_queens=self.queens
-        
-        for queen in list_queens:
+        while (self.is_solved()==False):
+
+            list_queens=self.queens
+            
+            new_queen=random.randint(0,self.n-1)
+            
+            queen=list_queens[new_queen]
+            
 
             moves=self.possible_moves(queen)
 
             min_conflicts= queen.conflicts
             min_pos= (queen.row,queen.col)
-        
+            # print("queen picked col=",queen.col)
+
+            
             for move in moves:
                 row=move[0]
                 col=move[1]
-                new_conflicts= self.current_queen_conflicts(row,col,queen)
+                new_conflicts= self.current_queen_conflicts(row,col)
 
-                if (new_conflicts<min_conflicts):
+                if (new_conflicts<=min_conflicts):
 
                     min_conflicts=new_conflicts
 
                     min_pos= (row,col)
 
-            self.make_move(min_pos[0],min_pos[1],queen)
+                # if (min_conflicts!=queen.conflicts):
 
             queen.conflicts=min_conflicts
-            print()
-            print("new position:")
-            print("row:",min_pos[0])
-            print("col:",min_pos[1])
-            print("new conflict",queen.conflicts)
 
+            self.make_move(min_pos[0],min_pos[1],queen)
+
+            
+                
+            # print()
+            # print("new position:")
+            # print("row:",min_pos[0])
+            # print("col:",min_pos[1])
+            # print("New Conflict:",queen.conflicts)
+                # if (min_conflicts==0):
+                #     print("added to solved")
+                #     solved+=1
+            max_steps+=1
+
+        print('DONE')
 
 
         return
@@ -225,23 +249,46 @@ class Board:
     #     return
 
     def make_move(self,row,col,queen):
+        
+        if (row!=queen.row ):
+            print("THIS SHOULD CHANGE TABLE")
+            
 
-        self.table[row][col]=1
-        self.table[queen.row][queen.col]=0
-        queen.row=row
-        queen.col=col
+            self.table[row][col]=1
+            self.table[queen.row][queen.col]=0
+            queen.row=row
+            queen.col=col
+            self.update_conflicts()
+            self.printBoard()
         return
 
     def possible_moves(self,queen):
-        row=queen.row
+        
         col=queen.col
         moves=[]
 
         for i in range (self.n):
-            if (i!=row):
+            if (i!=queen.row):
+
                 moves.append((i,col))
 
         return moves
+
+    def is_solved(self):
+        is_solved=False
+
+        conflicts_finished=0
+
+        for i in range(len(self.queens)):
+            if (self.queens[i].conflicts==0):
+                conflicts_finished+=1
+
+        if (conflicts_finished==self.n):
+            is_solved=True
+
+
+
+        return is_solved
 
 
 
@@ -254,19 +301,27 @@ class Board:
     
 
 def main():
-    board=Board(8)
+    print("\033[1;37;40m Bright Colour\033[0;37;40m \n")
+
+    board=Board(4)
 
     board.printBoard()
 
     board.update_conflicts()
 
-    print("POSSIBLE MOVES FOR QUEEN")
-    print(board.possible_moves(board.queens[0]))
+    # print("POSSIBLE MOVES FOR QUEEN")
+    # print(board.possible_moves(board.queens[0]))
 
-    print()
+    print("-----------------------------------------------------")
     print("MIN_CONFLICTS")
 
     board.min_conflicts(0)
+    board.printBoard()
+
+    print("Board is solved?: ",board.is_solved())
+    
+    
+
 
    
 
